dee1c79cacae201b84f2aaf43cbc8dbc2c7f7482
Sameer Faridi <s.faridi007@gmail.com>
2024-05-26 15:33:08 +0530

    Tabs Composition start

--- src/All Assessment/assessment09/Nine.jsx
+++ src/All Assessment/assessment09/Nine.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const Nine = () => {
   return (
     <div>
-      {/* main content */}
-    </div>
-  )
-}
+      {/* Tabs Composition */}
+      <div className="flex justify-center items-center h-screen">
 
-export default Nine
+      <First/>
+      <Secound/>
+      <Third/>
+      </div>
+    </div>
+  );
+};
+
+export default Nine;
+
+const First = () => {
+  return (
+    <div className=" min-h-72 w-96 border-2 border-black p-10">
+      <h1>
+        Join us for React JS Interview Custom Tabs React App-React JS Interview
+        Challenge #22 as we tackle a real-world problem in this Bar Chart
+        Component Suggest Machine Coding Round. Sharpen your React skills and
+        coding prowess with this engaging challenge! 🚀
+      </h1>
+    </div>
+  );
+};
+const Secound = () => {
+  return (
+    <div className="min-h-72 w-96 border-2 border-black p-10">
+      <h1>
+        Join us for React JS Interview Custom Tabs React App-React JS Interview
+        Challenge #22 as we tackle a real-world problem in this Bar Chart
+        Component Suggest Machine Coding Round. Sharpen your React skills and
+        coding prowess with this engaging challenge! 🚀
+      </h1>
+    </div>
+  );
+};
+const Third = () => {
+  return (
+    <div className="min-h-72 w-96 border-2 border-black p-10">
+      <h1>
+        Join us for React JS Interview Custom Tabs React App-React JS Interview
+        Challenge #22 as we tackle a real-world problem in this Bar Chart
+        Component Suggest Machine Coding Round. Sharpen your React skills and
+        coding prowess with this engaging challenge! 🚀
+      </h1>
+    </div>
+  );
+};
