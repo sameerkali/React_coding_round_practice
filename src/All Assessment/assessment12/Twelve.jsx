ad979ce14e9f505ee09e0a2cd466809bb010774d
Sameer Faridi <s.faridi007@gmail.com>
2024-05-28 13:05:00 +0530

    compleate : Blink game (Rowdi Coder)

--- src/All Assessment/assessment12/Twelve.jsx
+++ src/All Assessment/assessment12/Twelve.jsx
@@ -2,23 +2,41 @@ import React, { useEffect, useState } from "react";
 
 const Twelve = () => {
   const [blink, setBlink] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [fail, setFail] = useState(null);
+  const [score, setScore] = useState(0);
+
+
+  const handlePress = n => {
+    if (n === blink){
+      setSuccess(n);
+      setScore(prev => prev+1)
+    } 
+    else setFail(n)
+  };
 
   // blink random box logic
   let interval;
   const randomBlink = () => {
     interval = setInterval(() => {
-      setBlink(Math.floor(Math.random() * 10) % 5);
-    }, 800);
+      setBlink(parseInt(Math.random() * 10) % 5);
+      setSuccess(null)
+      setFail(null)
+    }, 2000);
   };
+
   useEffect(() => {
     return () => {
       clearInterval(interval);
-      randomBlink();
     };
   }, []);
+
   useEffect(() => {
-    
-  }, []);
+    const highScore = localStorage.getItem("blinkClick");
+    if (highScore === null || score > parseInt(highScore)) {
+      localStorage.setItem("blinkClick", score);
+    }
+  }, [score]);
 
   // Create boxes logic
   const boxes = [];
@@ -26,9 +44,13 @@ const Twelve = () => {
     boxes.push(
       <div
         key={i}
-        className={`h-20 w-20  border-gray-800 border-2 ${blink === i
-          ? "bg-red-800"
-          : ""}`}
+        onClick={() => handlePress(i)}
+        className={`h-36 w-36  border-gray-800 border-2 ${blink === i
+          ? "bg-blue-800"
+          : ""}
+          ${success === i ? "bg-green-800" : ""}
+          ${fail === i ? "bg-red-800": ""}
+          `}
       />
     );
   }
@@ -37,8 +59,19 @@ const Twelve = () => {
     <div className="flex flex-col h-screen items-center justify-center gap-10">
       <p className="">Blink game (Rowdi Coder)</p>
       <div className="flex gap-10">
+      <p>Score: {score}</p>
+      <p>High Score: {localStorage.getItem("blinkClick")}</p>
+      </div>
+      <div className="flex gap-1">
         {boxes}
       </div>
+      <button
+        onClick={randomBlink}
+        className="px-7 py-2 bg-gray-600 text-white"
+      >
+        Start
+      </button>
+      
     </div>
   );
 };
