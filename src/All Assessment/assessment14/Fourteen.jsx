4a0c059bb37e4968c58d6a69ba04c732f15669c7
Sameer Faridi <s.faridi007@gmail.com>
2024-06-12 22:22:40 +0530

    change in otp solution

--- src/All Assessment/assessment14/Fourteen.jsx
+++ src/All Assessment/assessment14/Fourteen.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import OtpForm from "./OtpForm";
 
 const Fourteen = () => {
   return (
     <div>
       <div>
         <p className="">OTP login</p>
+        <OtpForm/>
       </div>
     </div>
   );
---
+++ src/All Assessment/assessment14/OtpForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OtpForm = () => {
+  return (
+    <div>OtpForm</div>
+  )
+}
+
+export default OtpForm
