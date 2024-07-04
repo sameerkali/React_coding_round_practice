4097b1d1e1af089d97ce2034d10a165b5ff7e948
Sameer Faridi <s.faridi007@gmail.com>
2024-07-05 00:11:16 +0530

    add: button to link all problems

--- src/Components/allProblems/ProblemsPage.jsx
+++ src/Components/allProblems/ProblemsPage.jsx
@@ -1,66 +1,64 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 const categories = [
   {
-    name: 'Recent',
+    name: "Recent",
     posts: [
       {
         id: 1,
-        title: 'Does drinking coffee make you smarter?',
-        date: '5h ago',
+        title: "Does drinking coffee make you smarter?",
+        date: "5h ago",
         commentCount: 5,
-        shareCount: 2,
+        shareCount: 2
       },
       {
         id: 2,
         title: "So you've bought coffee... now what?",
-        date: '2h ago',
+        date: "2h ago",
         commentCount: 3,
-        shareCount: 2,
-      },
-    ],
+        shareCount: 2
+      }
+    ]
   },
   {
-    name: 'Popular',
+    name: "Popular",
     posts: [
       {
         id: 1,
-        title: 'Is tech making coffee better or worse?',
-        date: 'Jan 7',
+        title: "Is tech making coffee better or worse?",
+        date: "Jan 7",
         commentCount: 29,
-        shareCount: 16,
+        shareCount: 16
       },
       {
         id: 2,
-        title: 'The most innovative things happening in coffee',
-        date: 'Mar 19',
+        title: "The most innovative things happening in coffee",
+        date: "Mar 19",
         commentCount: 24,
-        shareCount: 12,
-      },
-    ],
+        shareCount: 12
+      }
+    ]
   },
   {
-    name: 'Trending',
+    name: "Trending",
     posts: [
       {
         id: 1,
-        title: 'Ask Me Anything: 10 answers to your questions about coffee',
-        date: '2d ago',
+        title: "Ask Me Anything: 10 answers to your questions about coffee",
+        date: "2d ago",
         commentCount: 9,
-        shareCount: 5,
+        shareCount: 5
       },
       {
         id: 2,
         title: "The worst advice we've ever heard about coffee",
-        date: '4d ago',
+        date: "4d ago",
         commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-  },
-]
-
-
+        shareCount: 2
+      }
+    ]
+  }
+];
 
 export default function ProblemsPage() {
   return (
@@ -68,40 +66,60 @@ export default function ProblemsPage() {
       <div className="w-full max-w-md">
         <TabGroup>
           <TabList className="flex gap-4">
-            {categories.map(({ name }) => (
+            {categories.map(({ name }) =>
               <Tab
                 key={name}
                 className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
               >
                 {name}
               </Tab>
-            ))}
+            )}
           </TabList>
           <TabPanels className="mt-3">
-            {categories.map(({ name, posts }) => (
+            {categories.map(({ name, posts }) =>
               <TabPanel key={name} className="rounded-xl bg-white/5 p-3">
                 <ul>
-                  {posts.map((post) => (
-                    <li key={post.id} className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5">
+                  {posts.map(post =>
+                    <li
+                      key={post.id}
+                      className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5"
+                    >
                       <a href="#" className="font-semibold text-white">
                         <span className="absolute inset-0" />
                         {post.title}
                       </a>
-                      <ul className="flex gap-2 text-white/50" aria-hidden="true">
-                        <li>{post.date}</li>
+                      <ul
+                        className="flex gap-2 text-white/50"
+                        aria-hidden="true"
+                      >
+                        <li>
+                          {post.date}
+                        </li>
                         <li aria-hidden="true">&middot;</li>
-                        <li>{post.commentCount} comments</li>
+                        <li>
+                          {post.commentCount} comments
+                        </li>
                         <li aria-hidden="true">&middot;</li>
-                        <li>{post.shareCount} shares</li>
+                        <li>
+                          {post.shareCount} shares
+                        </li>
                       </ul>
                     </li>
-                  ))}
+                  )}
                 </ul>
               </TabPanel>
-            ))}
+            )}
           </TabPanels>
         </TabGroup>
       </div>
+      {/* <iframe
+        src="https://github.com/sponsors/sameerkali/button"
+        title="Sponsor sameerkali"
+        height="32"
+        width="114"
+        className="absolute top-[-4rem] left-[1rem]"
+        style={{ border: "0", borderRadius: "6px", marginTop: "5rem" }}
+      /> */}
     </div>
-  )
+  );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./index.css";
 import Card from "./Components/Card";
@@ -11,31 +10,19 @@ const Home = () => {
   return (
     //Black UI
     <div className="bwgradient min-h-screen flex flex-col items-center justify-center p-4 ">
-    {/*  <div className="home min-h-screen flex flex-col items-center justify-center p-4 "> */}
-      <Marquee/>
+      {/*  <div className="home min-h-screen flex flex-col items-center justify-center p-4 "> */}
+      <Marquee />
       <div>
-      {/* <Accordion  /> */}
-    </div>
-      <iframe
-        src="https://github.com/sponsors/sameerkali/button"
-        title="Sponsor sameerkali"
-        height="32"
-        width="114"
-        style={{ border: "0", borderRadius: "6px", marginTop: "5rem" }}
-      />
+        {/* <Accordion  /> */}
+      </div>
 
-      <a
-        href="https://github.com/sameerkali/React_coding_round_practice"
-        target="_blank"
-        rel="noopener noreferrer"
-        className=" mt-5 mb-8 text-gray-300 bg-[#21262d] px-3 py-1 rounded-md no-underline hover:no-underline hover:bg-[#21262Dc3]"
-      >
-        ⭐ Star on GitHub
-      </a>
-
-      <h1 className="text-4xl sm:text-5xl md:text-6xl text-center font-bold mb-8 text-[#a29bfe]">
+      {/* <h1 className="text-4xl sm:text-5xl md:text-6xl text-center font-bold mb-8 text-[#a29bfe]">
+        Practice Interview Problems
+      </h1> */}
+      <h1 class="text-4xl sm:text-5xl md:text-6xl text-center font-bold mb-8 bg-gradient-to-r from-[#9A8D9B]   via-[#575A79]  to-[#2C2E4D] inline-block text-transparent bg-clip-text">
         Practice Interview Problems
       </h1>
+
       <div className="flex justify-center w-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-6xl justify-items-center mb-10">
           {buttonDetails.map(({ name, description }) =>
@@ -48,8 +35,33 @@ const Home = () => {
           )}
         </div>
       </div>
+      {/* <div className="flex gap-10 bg-transparent border border-gray-400 rounded-lg font-mono px-16 py-2">
+          <h1>All problems</h1>
+          <img className="h-7 w-10" src="/direction.svg" />
+        </div> */}
       <Link to={`/problems`}>
-        <h1>All problems</h1>
+        <a class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-gray-500 transition duration-300 ease-out border-2 border-slate-700 rounded-full shadow-md group w-72">
+          <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              />
+            </svg>
+          </span>
+          <span class="absolute flex items-center justify-center w-full h-full text-gray-200 transition-all duration-300 transform group-hover:translate-x-full ease">
+            All problems
+          </span>
+          <span class="relative invisible">All problems</span>
+        </a>
       </Link>
     </div>
   );
