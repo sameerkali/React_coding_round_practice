84cf6f063b3eb99ab3612b25ff181d1189d3e73c
Sameer Faridi <s.faridi007@gmail.com>
2024-08-20 22:00:13 +0530

    Done: FollowingCurser

--- src/All Assessment/assessment19/FollowingCurser.jsx
+++ src/All Assessment/assessment19/FollowingCurser.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useRef } from "react";
+import GoToHome from "../../Components/GoToHome";
+import Circle from "./Circle";
 
 const FollowingCurser = () => {
-  return (
-    <div>FollowingCurser</div>
-  )
-}
+  const circleRef = useRef();
+  const rootRef = useRef();
 
-export default FollowingCurser
+  const handleMovement = e => {
+    const circle = circleRef.current;
+    setTimeout(() => {
+      circle.style.left = `${e.clientX}px`;
+      circle.style.top = `${e.clientY}px`;
+    }, 100);
+  };
+
+  return (
+    <div
+      ref={rootRef}
+      className="flex h-screen items-center justify-center flex-col gap-10"
+      onMouseMove={handleMovement}
+    >
+      <div className="text-4xl font-sans">
+        The Circle That Follows The Curser
+      </div>
+      <Circle ref={circleRef} />
+      <GoToHome />
+    </div>
+  );
+};
+
+export default FollowingCurser;
