d1cd091e5dcd222228dc392189b6ef2691b85fc2
sameerkali <s.faridi007@gmail.com>
2025-01-04 14:13:49 +0530

    FormValidation

--- src/All Assessment/assessment24/FormValidation.jsx
+++ src/All Assessment/assessment24/FormValidation.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const FormValidation = () => {
+
+  const handleForm = () => {
+    alert("sameer")
+  }
   return (
-    <div>FormValidation</div>
+    <div className='h-screen flex justify-center items-center'>
+      <div className='flex flex-col gap-4 '>
+        <input type='text' placeholder='name' className='border-2 pl-2' />
+        <input  type='email' placeholder='abc@xyz.com' className='border-2 pl-2'/>
+        <input  type='password' className='border-2 pl-2' />
+        <button onClick={handleForm}>Submit</button>
+      </div>
+    </div>
   )
 }
 
