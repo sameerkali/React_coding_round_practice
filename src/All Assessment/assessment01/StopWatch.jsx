a628ce867ecb261baad3669d329bba497aff69ce
sameerkali <s.faridi007@gmail.com>
2025-01-07 17:27:47 +0530

    StopWatch

--- src/All Assessment/assessment01/StopWatch.jsx
+++ src/All Assessment/assessment01/StopWatch.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
-import '../../index.css'
+import "../../index.css";
 import GoToHome from "../../Components/GoToHome";
 
 const StopWatch = () => {
   const [clicked, setClicked] = useState(false);
   const [time, setTime] = useState(0);
 
- 
-
   useEffect(() => {
     let interval;
     if (clicked) {
       interval = setInterval(() => {
-        setTime(prevTime => prevTime + 1);
+        setTime((prevTime) => prevTime + 1);
       }, 500);
     } else {
       clearInterval(interval);
@@ -24,13 +22,10 @@ const StopWatch = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-10">
       <div className="text-6xl">{time}</div>
-      <button
-        className="custom-button"
-        onClick={() => setClicked(!clicked)}
-      >
-        {clicked ? 'Stop' : 'Start'}
+      <button className="custom-button" onClick={() => setClicked(!clicked)}>
+        {clicked ? "Stop" : "Start"}
       </button>
-      <GoToHome/>
+      <GoToHome />
     </div>
   );
 };
